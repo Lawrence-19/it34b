77d7c48732f73a76adf3c9584134ace98eb5a367
Lawrence-19 <lawrencesombreno@gmail.com>
2026-08-24 15:54:02 +0800

    added activity logger and test logger

--- db/schema.sql
+++ db/schema.sql
@@ -1,33 +1,14 @@
-CREATE TABLE users(
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_email VARCHAR(255)  UNIQUE NOT NULL,
-    user_password VARCHAR(255) NOT NULL,
-    user_role ENUM('admin', 'manager', 'user') DEFAULT 'user',
+CREATE TABLE if NOT EXISTS activity_logs(
+    activity_log_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(50),
+    user_email VARCHAR(50),
+    activity_log_action VARCHAR(50) NOT NULL,
+    activity_log_status ENUM('success', 'failed') DEFAULT 'success',
 
-    -- Email Verification Fields
-    user_is_verified TINYINT(1) DEFAULT 0,
-    user_verification_token VARCHAR(64) DEFAULT NULL,
-    user_email_verification_expires DATETIME DEFAULT NULL,
+    -- Client Parameters
+    activity_log_ip_address VARCHAR(45),
+    activity_log_user_agent VARCHAR(255),
 
-    -- Date Parameters
-    user_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    user_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    -- timestamp
+    activity_log_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
-
-CREATE TABLE if NOT EXISTS user_activity_logs(
-    user_activity_log_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NULL,
-    user_email VARCHAR(255),
-    user_activity_log_action VARCHAR(50) NOT NULL,
-    user_activity_log_status ENUM('success', 'failed') DEFAULT 'success',
-
-    --Client Parameters
-    user_activity_log_ip_address VARCHAR(45),
-    user_activity_log_user VARCHAR(255),
-    user_activity_log_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-    -- Indexes
-    INDEX idx_user_id (user_id),
-    INDEX idx_action(user_activity_log_action),
-    INDEX idx_created_at (user_activity_log_created_at)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- includes/index.php
+++ includes/index.php
@@ -25,6 +25,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     typr="submit"
     name="action"
     >Sample</button>
-    
+</form>
+
 </body>
 </html>
